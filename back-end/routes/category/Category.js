7cacebf8e08c38292ac5a9a5a0fbf13b50fb2a9e
manish0521 <manish.singh@codeimmersives.com>
2019-11-06 14:27:14 -0500

    schema modified with routes

--- back-end/routes/category/Category.js
+++ back-end/routes/category/Category.js
@@ -2,14 +2,22 @@ const mongoose = require('mongoose');
 const moment = require("moment");
 const now = moment();
 
-let CategorySchema = mongoose.Schema({
+// let CategorySchema = mongoose.Schema({
     
-      user_id: { type: mongoose.SchemaTypes.ObjectId, ref: "user"}, 
-      politics: { type: mongoose.SchemaTypes.ObjectId, ref: "politics"}, 
-      sports: { type: mongoose.SchemaTypes.ObjectId, ref: "sports"}, 
-      general: { type: mongoose.SchemaTypes.ObjectId, ref: "genral"}, 
-      timestamp: { type: String, default: now.format("dddd, MMMM Do YYYY, kk:mm:ss") }
+//       user_id: { type: mongoose.SchemaTypes.ObjectId, ref: "user"}, 
+//       politics: { type: String , ref: "topic", unique: true, required: true,default: ''},
+//       sports: { type: String, ref: "topic", unique: true, required: true,  default: ''},
+//       veterans: { type: String, ref: "topic", unique: true, required: true, default: '' },
+//       timestamp: {
+//         type: String, default: now.format("dddd, MMMM Do YYYY, kk:mm:ss")
+//       }
 
+
+// })
+
+let CategorySchema = mongoose.Schema({
+    name: { type: String, default: ''},
+    topics: { type: mongoose.SchemaType.ObjectId, ref: 'topic'}
 })
 
 module.exports = mongoose.model('Category', CategorySchema)
